d970177bec8703f9b748e1937e0bbe2885dd0e2d
GianlucaProjects <134298649+GianlucaProjects@users.noreply.github.com>
2024-07-31 18:29:50 +0200

    commit
    commit per player spotify

--- assets/js/album.js
+++ assets/js/album.js
@@ -1,9 +1,23 @@
-const URL = "https://striveschool-api.herokuapp.com/api/deezer/album/";
+// FUNZIONE X CREAZIONE PLAYLIST
+function createPlaylist() {
+  const playlist = prompt(
+    "Scrivi di seguito il nome della playlist",
+    "ES: Summer 2K21"
+  );
 
-/*const addressBarParameters = new URLSearchParams(location.search);
-const urlAlbumId = addressBarParameters.get("albumId");
-console.log("L'albumId è", urlAlbumId);
-*/
+  // localStorage.setItem("nome playlist", playlist)
+  // const localPlay = localStorage.getItem(playlist)
+
+  const listOfPlay = document.getElementById("playlist-index");
+  const node = document.createElement("li");
+  node.classList.add("pt-2");
+  node.textContent = `${playlist}`;
+  listOfPlay.appendChild(node);
+}
+
+// FUNZIONE X PLAYER SPOTIFY
+let track_index = 0;
+const URL = "https://striveschool-api.herokuapp.com/api/deezer/album/";
 
 const getAlbum = function () {
   fetch(URL + 108564)
@@ -15,68 +29,31 @@ const getAlbum = function () {
       }
     })
     .then((album) => {
-      console.log(album);
-
-      document.getElementById("album-img").src = album.cover_medium;
-      document.getElementById("album-title").innerText = album.title;
-      document.getElementById("album-artist-name").innerText =
-        album.artist.name;
-      document.getElementById("album-release-date").innerText = new Date(
-        album.release_date
-      ).getFullYear();
-      document.getElementById("tracks-number").innerText =
-        album.tracks.data.length + " Brani, ";
-      document.getElementById("album-duration").innerText =
-        formatSecondsWithletters(album.duration);
-
       let songs = album.tracks.data;
+      let track_list = createArray(songs);
 
-      songs.forEach((song) => {
-        // song.preview
-        document.getElementById("songs").innerHTML += `
-            <div class="row px-4 py-2" id="songs" onClick="startPreview('${
-              song.preview
-            }')">
-                <div class="d-flex  align-items-center fw-lighter">
-                    <div class="col-6 mb-2 d-flex align-items-center">
-                    <div class="me-3">
-                        <span>${songs.indexOf(song) + 1}</span>
-                    </div>
-                    <div class="d-flex flex-column">
-                        <span class="fw-bold">${song.title}</span>
-                        <span>${album.artist.name}</span>
-                    </div>
-                    </div>
-                    <div class="col-3 d-flex justify-content-end">
-                    <span>${song.id}</span>
-                    </div>
-                    <div class="col-3 d-flex justify-content-end">
-                    <span>${formatSeconds(song.duration)}</span>
-                    </div>
-                </div>
-            </div>
-            
-        `;
-      });
-      document.querySelector("main").innerHTML += `
-        </div>
-        </div>
-        </div>
-        </div>
-        </div>`;
+      // Load the first track in the tracklist
+      // loadTrack(track_index, track_list);
+
+      displaySongs(songs, track_list);
     })
     .catch((error) => {
       console.log("ERRORE!", error);
     });
 };
 
-function formatSecondsWithletters(seconds) {
-  let minutes = Math.floor(seconds / 60);
-  let secondsLeft = seconds % 60;
+function createArray(songs) {
+  console.log(songs);
+  let track_list = [];
+  songs.forEach((song) => {
+    track_list.push({
+      name: song.title,
+      path: song.preview,
+      artist: song.artist.name,
+    });
+  });
 
-  secondsLeft = secondsLeft < 10 ? "0" + secondsLeft : secondsLeft;
-
-  return minutes + " min " + secondsLeft + " sec";
+  return track_list;
 }
 
 function formatSeconds(seconds) {
@@ -88,17 +65,66 @@ function formatSeconds(seconds) {
   return minutes + ":" + secondsLeft;
 }
 
-function startPreview(previewUrl) {
-  const audio = new Audio(previewUrl);
-  audio.play();
+/*
+
+function displaySongs(songs, track_list) {
+  songs.forEach((song) => {
+    // song.preview
+    document.getElementById("songs").innerHTML += `
+        <div class="row px-4 py-2" onClick="loadTrack(${songs.indexOf(song)}, ${track_list})">
+            <div class="d-flex  align-items-center fw-lighter">
+                <div class="col-6 mb-2 d-flex align-items-center">
+                <div class="me-3">
+                    <span>${songs.indexOf(song) + 1}</span>
+                </div>
+                <div class="d-flex flex-column">
+                    <span class="fw-bold">${song.title}</span>
+                    <span>${song.artist.name}</span>
+                </div>
+                </div>
+                <div class="col-3 d-flex justify-content-end">
+                <span>${song.id}</span>
+                </div>
+                <div class="col-3 d-flex justify-content-end">
+                <span>${formatSeconds(song.duration)}</span>
+                </div>
+            </div>
+        </div>
+    `;
+  });
+}*/
+
+function displaySongs(songs, track_list) {
+  const songsContainer = document.getElementById("songs");
+  songs.forEach((song, index) => {
+      const songDiv = document.createElement("div");
+      songDiv.classList.add("row", "px-4", "py-2");
+      songDiv.dataset.index = index;
+      songDiv.innerHTML = `
+          <div class="d-flex align-items-center fw-lighter">
+              <div class="col-6 mb-2 d-flex align-items-center">
+                  <div class="me-3">
+                      <span>${index + 1}</span>
+                  </div>
+                  <div class="d-flex flex-column">
+                      <span class="fw-bold">${song.title}</span>
+                      <span>${song.artist.name}</span>
+                  </div>
+              </div>
+              <div class="col-3 d-flex justify-content-end">
+                  <span>${song.id}</span>
+              </div>
+              <div class="col-3 d-flex justify-content-end">
+                  <span>${formatSeconds(song.duration)}</span>
+              </div>
+          </div>
+      `;
+      songDiv.addEventListener("click", () => loadTrack(index, track_list));
+      songsContainer.appendChild(songDiv);
+  });
 }
 
-// FUNZIONE X PLAYER SPOTIFY
-
-let now_playing = document.querySelector(".now-playing");
-let track_art = document.querySelector(".track-art");
-let track_name = document.querySelector(".track-name");
-let track_artist = document.querySelector(".track-artist");
+// let now_playing = document.querySelector(".now-playing");
 
 let playpause_btn = document.querySelector(".playpause-track");
 let next_btn = document.querySelector(".next-track");
@@ -109,49 +135,25 @@ let volume_slider = document.querySelector(".volume_slider");
 let curr_time = document.querySelector(".current-time");
 let total_duration = document.querySelector(".total-duration");
 
-let track_index = 0;
 let isPlaying = false;
 let updateTimer;
 
 // Create new audio element
-let curr_track = document.createElement('audio');
+let curr_track = document.createElement("audio");
 
-// Define the tracks that have to be played
-let track_list = [
-  {
-    name: "Night Owl",
-    artist: "Broke For Free",
-    image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
-    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3"
-  },
-  {
-    name: "Enthusiast",
-    artist: "Tours",
-    image: "https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
-    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Tours/Enthusiast/Tours_-_01_-_Enthusiast.mp3"
-  },
-  {
-    name: "Shipping Lanes",
-    artist: "Chad Crouch",
-    image: "https://images.pexels.com/photos/1717969/pexels-photo-1717969.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
-    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Shipping_Lanes.mp3",
-  },
-];
-
-
-function loadTrack(track_index) {
+function loadTrack(track_index, track_list) {
+  console.log("qui dovresti caricare canzone!!")
   clearInterval(updateTimer);
   resetValues();
   curr_track.src = track_list[track_index].path;
   curr_track.load();
 
-  track_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
-  track_name.textContent = track_list[track_index].name;
-  track_artist.textContent = track_list[track_index].artist;
-  now_playing.textContent = "PLAYING " + (track_index + 1) + " OF " + track_list.length;
+  // now_playing.textContent =
+  //  "PLAYING " + (track_index + 1) + " OF " + track_list.length;
 
   updateTimer = setInterval(seekUpdate, 1000);
   curr_track.addEventListener("ended", nextTrack);
+  playTrack();
 }
 
 function resetValues() {
@@ -159,9 +161,6 @@ function resetValues() {
   total_duration.textContent = "00:00";
   seek_slider.value = 0;
 }
-
-// Load the first track in the tracklist
-loadTrack(track_index);
 
 function playpauseTrack() {
   if (!isPlaying) playTrack();
@@ -171,26 +170,26 @@ function playpauseTrack() {
 function playTrack() {
   curr_track.play();
   isPlaying = true;
-  playpause_btn.innerHTML = '<i role="button" class="bi bi-pause-circle-fill fs-2"></i>';
+  playpause_btn.innerHTML =
+    '<i role="button" class="bi bi-pause-circle-fill fs-2"></i>';
 }
 
 function pauseTrack() {
   curr_track.pause();
   isPlaying = false;
-  playpause_btn.innerHTML = '<i role="button" class="bi bi-play-circle-fill fs-2"></i>';;
+  playpause_btn.innerHTML =
+    '<i role="button" class="bi bi-play-circle-fill fs-2"></i>';
 }
 
 function nextTrack() {
-  if (track_index < track_list.length - 1)
-    track_index += 1;
+  if (track_index < track_list.length - 1) track_index += 1;
   else track_index = 0;
   loadTrack(track_index);
   playTrack();
 }
 
 function prevTrack() {
-  if (track_index > 0)
-    track_index -= 1;
+  if (track_index > 0) track_index -= 1;
   else track_index = track_list.length;
   loadTrack(track_index);
   playTrack();
@@ -214,18 +213,56 @@ function seekUpdate() {
     seek_slider.value = seekPosition;
 
     let currentMinutes = Math.floor(curr_track.currentTime / 60);
-    let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+    let currentSeconds = Math.floor(
+      curr_track.currentTime - currentMinutes * 60
+    );
     let durationMinutes = Math.floor(curr_track.duration / 60);
-    let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+    let durationSeconds = Math.floor(
+      curr_track.duration - durationMinutes * 60
+    );
 
-    if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
-    if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
-    if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
-    if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+    if (currentSeconds < 10) {
+      currentSeconds = "0" + currentSeconds;
+    }
+    if (durationSeconds < 10) {
+      durationSeconds = "0" + durationSeconds;
+    }
+    if (currentMinutes < 10) {
+      currentMinutes = "0" + currentMinutes;
+    }
+    if (durationMinutes < 10) {
+      durationMinutes = "0" + durationMinutes;
+    }
 
     curr_time.textContent = currentMinutes + ":" + currentSeconds;
     total_duration.textContent = durationMinutes + ":" + durationSeconds;
   }
 }
+
+// Define the tracks that have to be played
+/*
+let track_list = [
+  {
+    name: "Night Owl",
+    artist: "Broke For Free",
+    image:
+      "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3",
+  },
+  {
+    name: "Enthusiast",
+    artist: "Tours",
+    image:
+      "https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Tours/Enthusiast/Tours_-_01_-_Enthusiast.mp3",
+  },
+  {
+    name: "Shipping Lanes",
+    artist: "Chad Crouch",
+    image:
+      "https://images.pexels.com/photos/1717969/pexels-photo-1717969.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Shipping_Lanes.mp3",
+  },
+];*/
 
 getAlbum();
