9946957296d7a14aad30b865ef866ccc1c9186ed
xAldox995 <valzanialdo@gmail.com>
2024-08-01 18:33:24 +0200

    aggiusti

--- assets/js/artist.js
+++ assets/js/artist.js
@@ -192,19 +192,19 @@ const showMoreTracks = function () {
 
 
 // FUNZIONE X APPARSA/SCOMPARSA ATTIVITA AMICI
-// function modalNostrum() {
-//   const test1 = document.getElementById("test_01");
-//   const test2 = document.getElementById("test_02");
+function modalNostrum() {
+  const test1 = document.getElementById("test_01");
+  const test2 = document.getElementById("test_02");
 
 
-//   if (test2.classList.contains("col-10")) {
-//     test2.classList.remove("col-10");
-//     test2.classList.add("col-8");
-//     test1.classList.toggle("d-none");
+  if (test2.classList.contains("col-10")) {
+    test2.classList.remove("col-10");
+    test2.classList.add("col-8");
+    test1.classList.toggle("d-none");
 
-//   } else {
-//     test2.classList.remove("col-8");
-//     test2.classList.add("col-10");
-//     test1.classList.toggle("d-none");
-//   }
-// }
+  } else {
+    test2.classList.remove("col-8");
+    test2.classList.add("col-10");
+    test1.classList.toggle("d-none");
+  }
+}
