f1d5bfc63817c889fa4292299fd0685b20b4e14e
xAldox995 <valzanialdo@gmail.com>
2024-08-01 16:43:34 +0200

    abozzo di soluzione per button

---
+++ assets/js2/artist.js
@@ -0,0 +1,200 @@
+const urlArtist =
+  "https://striveschool-api.herokuapp.com/api/deezer/artist/412";
+// console.log(urlArtist);
+
+const infoArtist = function () {
+  fetch(urlArtist)
+    .then((response) => {
+      // console.log(response);
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("La response non è ok");
+      }
+    })
+    .then((dataArtist) => {
+      // console.log(dataArtist);
+      populateArtistInfo(dataArtist);
+      return fetch(dataArtist.tracklist);
+    })
+    .then((response) => {
+      // console.log(response);
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error('Ci sei quasi')
+      }
+    })
+    .then((dataTrack) => {
+      // console.log(dataTrack)
+      top50Songs = populateTracks(dataTrack)
+      showMoreTracks(top50Songs)
+    })
+
+    .catch((error) => {
+      console.log("Hai sbagliato qualcosa", error);
+    });
+};
+
+infoArtist()
+
+
+let top50Songs = [];
+let songsCount = 0
+
+const populateArtistInfo = function (dataArtist) {
+  const imageBackground = document.getElementById("imageBackground");
+  imageBackground.style.backgroundImage = `url(${dataArtist.picture_xl})`;
+  const artistTitle = document.getElementById('artistTitle')
+  artistTitle.innerText = dataArtist.name
+  const artistFan = document.getElementById('artistFan')
+  artistFan.innerText = `${dataArtist.nb_fan} ascoltatori mensili`
+  const imgSm = document.getElementById('imgSm')
+  imgSm.innerHTML = `
+  <img src="${dataArtist.picture_xl}" alt="img a caso" class="w-25 me-2">
+                      <div>
+                      <h6> Hai messo Mi piace a tot brani </h6>
+                      <p>Di ${dataArtist.name}</p>
+                    </div>
+  `
+}
+
+function formatSeconds(seconds) {
+  let minutes = Math.floor(seconds / 60);
+  let secondsLeft = seconds % 60;
+
+  secondsLeft = secondsLeft < 10 ? "0" + secondsLeft : secondsLeft;
+
+  return minutes + ":" + secondsLeft;
+}
+
+const populateTracks = function (dataTrack) {
+  top50Songs = dataTrack.data
+  console.log(top50Songs)
+  songsCount = 0
+  loadSongs(5)
+  return top50Songs;
+}
+
+const loadSongs = function (contatore) {
+  const songsList = document.getElementById('songsList')
+  for (let i = 0; i < top50Songs.length && i < songsCount + contatore; i++) {
+    let song = top50Songs[i]
+    let artistName = song.artist.name
+    let imageAlbum = song.album.cover_small;
+    let titleAlbum = song.title_short;
+    let albumId = song.id;
+    let durationSong = formatSeconds(song.duration)
+    console.log(songsList)
+
+    songsList.innerHTML += `
+      <div class="row px-4 py-2">
+       <div class="d-flex  align-items-center fw-lighter">
+         <div class="col-6 mb-2 d-flex align-items-center">
+           <div class="me-3">
+             <span>${i + 1}</span>
+             <img src=${imageAlbum} alt= imgAlbum
+           </div>
+           <div class="d-flex flex-column">
+             <span class="fw-bold">${titleAlbum}</span>
+             <span> ${artistName}</span>
+           </div>
+         </div>
+         <div class="col-3 d-flex justify-content-end">
+           <span>${albumId}</span>
+         </div>
+         <div class="col-3 d-flex justify-content-end">
+           <span>${durationSong}</span>
+         </div>
+       </div>
+     </div>
+    `
+    songsCount += contatore
+  }
+}
+
+const showMoreTracks = function (top50Songs) {
+const btnOfExpand = document.getElementById('btnOfExpand')
+btnOfExpand.addEventListener('click', function () {
+  loadSongs(5);
+})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// const showAndHidden = function (dataTrack) {
+//   console.log(dataTrack)
+//   top50Songs = dataTrack.data
+//   console.log(top50Songs)
+//   const songsList = document.getElementById('songsList')
+
+//   for (let i = 0; i < top50Songs.length && i < 10; i++) {
+//     let song = top50Songs[i]
+//     let artistName = song.artist.name
+//     let imageAlbum = song.album.cover_small;
+//     let titleAlbum = song.title_short;
+//     let albumId = song.id;
+//     let durationSong = formatSeconds(song.duration)
+//     console.log(songsList)
+
+//     songsList.innerHTML += `
+//       <div class="row px-4 py-2">
+//        <div class="d-flex  align-items-center fw-lighter">
+//          <div class="col-6 mb-2 d-flex align-items-center">
+//            <div class="me-3">
+//              <span>${i + 1}</span>
+//              <img src=${imageAlbum} alt= imgAlbum
+//            </div>
+//            <div class="d-flex flex-column">
+//              <span class="fw-bold">${titleAlbum}</span>
+//              <span> ${artistName}</span>
+//            </div>
+//          </div>
+//          <div class="col-3 d-flex justify-content-end">
+//            <span>${albumId}</span>
+//          </div>
+//          <div class="col-3 d-flex justify-content-end">
+//            <span>${durationSong}</span>
+//          </div>
+//        </div>
+//      </div>
+//     `
+//   }
+// }
+
+// const Pippo = function () {
+//   const btnOfExpand = document.getElementById('btnOfExpand')
+//   showAndHidden(dataTrack)
+// }
+
+
+// btnOfExpand.addEventListener('click', Pippo)
+
+
+
+// FUNZIONE X APPARSA/SCOMPARSA ATTIVITA AMICI
+// function modalNostrum() {
+//   const test1 = document.getElementById("test_01");
+//   const test2 = document.getElementById("test_02");
+
+
+//   if (test2.classList.contains("col-10")) {
+//     test2.classList.remove("col-10");
+//     test2.classList.add("col-8");
+//     test1.classList.toggle("d-none");
+
+//   } else {
+//     test2.classList.remove("col-8");
+//     test2.classList.add("col-10");
+//     test1.classList.toggle("d-none");
+//   }
+// }
