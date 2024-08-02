f9f4f5f18abd5a3904708da6385efc8ce74b796b
John-9813 <john.oliver98.br@gmail.com>
2024-08-02 16:20:28 +0200

    Update index.js

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -16,13 +16,13 @@ function modalNostrum() {
   const test1 = document.getElementById("test_01");
   const test2 = document.getElementById("test_02");
 
-  if (test2.classList.contains("col-10")) {
-    test2.classList.remove("col-10");
-    test2.classList.add("col-8");
+  if (test2.classList.contains("col-9")) {
+    test2.classList.remove("col-9");
+    test2.classList.add("col-7");
     test1.classList.toggle("d-none");
   } else {
-    test2.classList.remove("col-8");
-    test2.classList.add("col-10");
+    test2.classList.remove("col-7");
+    test2.classList.add("col-9");
     test1.classList.toggle("d-none");
   }
 }
@@ -201,3 +201,9 @@ doMultipleFetch(479176555);
 doMultipleFetch(112275);
 
 doFetch();
+doFetch(1121401);
+doFetch(664237);
+doFetch(13082922);
+doFetch(12279688);
+doFetch(112275);
+
