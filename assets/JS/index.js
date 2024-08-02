c0ec9ac0cacad277d2ef895a655d7cf700741943
John-9813 <john.oliver98.br@gmail.com>
2024-08-02 10:54:44 +0200

    Revert "commit delle 10:30"
    This reverts commit 0286b07b274cdf24793964ec8a8b56d691ae7fc6.

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -3,13 +3,13 @@ function modalNostrum() {
   const test1 = document.getElementById("test_01");
   const test2 = document.getElementById("test_02");
 
-  if (test2.classList.contains("col-9")) {
-    test2.classList.remove("col-9");
-    test2.classList.add("col-7");
+  if (test2.classList.contains("col-10")) {
+    test2.classList.remove("col-10");
+    test2.classList.add("col-8");
     test1.classList.toggle("d-none");
   } else {
-    test2.classList.remove("col-7");
-    test2.classList.add("col-9");
+    test2.classList.remove("col-8");
+    test2.classList.add("col-10");
     test1.classList.toggle("d-none");
   }
 }
